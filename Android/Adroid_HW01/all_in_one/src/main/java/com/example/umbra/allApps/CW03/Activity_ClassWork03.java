5c86c3712008501c9358161746ae8500dcba3624
sevodnia <sevodnia@yandex.ru>
2017-07-30 23:22:28 +0300

    HomeWork03_android
    HomeWork03_android is done!

---
+++ Android/Adroid_HW01/all_in_one/src/main/java/com/example/umbra/allApps/CW03/Activity_ClassWork03.java
@@ -0,0 +1,17 @@
+package com.example.umbra.allApps.CW03;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.example.umbra.allApps.R;
+
+public class Activity_ClassWork03 extends Activity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.cw3_activity);
+    }
+}
--- Android/Adroid_HW01/all_in_one/src/main/java/com/example/umbra/allApps/main/MainActivity.java
+++ Android/Adroid_HW01/all_in_one/src/main/java/com/example/umbra/allApps/main/MainActivity.java
@@ -1,16 +1,19 @@
 package com.example.umbra.allApps.main;
 
-import android.app.Activity;
-import android.content.Intent;
-import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.view.View;
-import android.widget.Button;
+        import android.app.Activity;
+        import android.content.Intent;
+        import android.os.Bundle;
+        import android.support.annotation.Nullable;
+        import android.view.View;
+        import android.widget.Button;
 
-import com.example.umbra.allApps.R;
-import com.example.umbra.allApps.CW02.Activity_ClassWork02;
-import com.example.umbra.allApps.HW01.Activity_HomeWork01;
-import com.example.umbra.allApps.HW02.Activity_HomeWork02;
+        import com.example.umbra.allApps.CW03.Activity_ClassWork03;
+        import com.example.umbra.allApps.HW03.Activity_HomeWork03;
+        import com.example.umbra.allApps.R;
+        import com.example.umbra.allApps.CW02.Activity_ClassWork02;
+        import com.example.umbra.allApps.HW01.Activity_HomeWork01;
+        import com.example.umbra.allApps.HW02.Activity_HomeWork02;
+
 /**
  * Created by Umbra on 26.07.17.
  */
@@ -21,8 +24,6 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.main_activity);
-
-
 
 
         //домашка №1
@@ -57,6 +58,35 @@ public class MainActivity extends Activity {
 
             }
         });
+
+//классная работа №3
+
+
+        Button cw3button = (Button) findViewById(R.id.cw3button);
+        cw3button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Activity_ClassWork03.class);
+                startActivity(intent);
+
+
+            }
+        });
+
+
+
+
+//        домашняя работа №3
+
+        Button hw3button  = (Button) findViewById(R.id.dz3button);
+        hw3button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Activity_HomeWork03.class);
+                startActivity(intent);
+            }
+        });
+
 
 
     }
